ec15d1a993cf677a7830ad796dca53212d73e516
Hamza Adeel <hamzaadeel621@gmail.com>
2025-10-09 11:51:39 +0500

    Adding local-host link in CORS

--- server.py
+++ server.py
@@ -16,7 +16,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://chatbot-ui-henna-rho.vercel.app/"],
+    allow_origins=["https://chatbot-ui-henna-rho.vercel.app/",
+                   "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
